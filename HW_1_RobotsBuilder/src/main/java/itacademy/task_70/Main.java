c674c3825bbf7df61f526e7b8514fcb37e1d755a
Roman Gulyako <romangulyako@gmail.com>
2024-10-28 12:20:19 +0300

    Добавил решение задания 70
    Создал класс Man с полями и методами, а в классе Main с помощью рефлексии получил данные о классе Man

---
+++ HW_1_RobotsBuilder/src/main/java/itacademy/task_70/Main.java
@@ -0,0 +1,49 @@
+package itacademy.task_70;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Main {
+    public final static String METHOD_NAME = "printInfo";
+
+    public static void main(String[] args) {
+        try {
+            Main main = new Main();
+            Class<?> mainClass = main.getClass();
+            Method printInfoMethod = mainClass.getDeclaredMethod(METHOD_NAME, Class.class);
+            String info = printInfoMethod.invoke(main,Man.class).toString();
+            System.out.println(info);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String printInfo(Class<?> someClass) {
+        Field[] fields = someClass.getDeclaredFields();
+        Method[] methods = someClass.getDeclaredMethods();
+        Constructor<?>[] constructors = someClass.getDeclaredConstructors();
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Fields:\n");
+        for (Field field : fields) {
+            builder.append(field.getName())
+                    .append("\n");
+        }
+
+        builder.append("Methods:\n");
+        for (Method method : methods) {
+            builder.append(method.getName())
+                    .append("\n");
+        }
+
+        builder.append("Constructors:\n");
+        for (Constructor<?> constructor : constructors) {
+            builder.append(constructor.getName())
+                    .append("\n");
+        }
+
+        return builder.toString();
+    }
+}
