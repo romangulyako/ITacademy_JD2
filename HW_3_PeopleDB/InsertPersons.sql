b55867bc5fac0d1382e64fc5c09e0b4236427db0
Roman Gulyako <romangulyako@gmail.com>
2024-10-30 14:21:57 +0300

    Скрипт добавления в таблицу данных

---
+++ HW_3_PeopleDB/InsertPersons.sql
@@ -0,0 +1,29 @@
+INSERT INTO people.person (
+  age,salary,passport,address,dateOfBirthday,
+  dateTimeCreate,timeToLunch,letter)
+VALUES (23,1530.5,'QWERT00001','Minsk City','2001-08-25',
+sysdate(), '11:15:00','Some big text1');
+
+INSERT INTO people.person (
+  age,salary,passport,address,dateOfBirthday,
+  dateTimeCreate,timeToLunch,letter)
+VALUES (20,1432.4,'QWERT00002','Brest','2004-03-25',
+sysdate(), '11:20:00','Some big text2');
+
+INSERT INTO people.person (
+  age,salary,passport,address,dateOfBirthday,
+  dateTimeCreate,timeToLunch,letter)
+VALUES (19,1530.5,'QWERT00003','Grodno','2005-03-10',
+sysdate(), '11:00:00','Some big text3');
+
+INSERT INTO people.person (
+  age,salary,passport,address,dateOfBirthday,
+  dateTimeCreate,timeToLunch,letter)
+VALUES (65,3000.0,'QWERT00004','Minsk City','1959-04-12',
+sysdate(), '10:50:00','Some big text4');
+
+INSERT INTO people.person (
+  age,salary,passport,address,dateOfBirthday,
+  dateTimeCreate,timeToLunch,letter)
+VALUES (28,2578.5,'QWERT00005','Minsk City','1996-03-13',
+sysdate(), '11:00:00','Some big text5');
